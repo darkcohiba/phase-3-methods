e783107afbfcaa7a850f29ffbecd19581f64b0ae
Sam <samuel.waters@gmail.com>
2022-04-04 09:29:03 -0600

    almost done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,19 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name= "programmer")
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name= "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(a, b)
+    return a + b
+end
+
+def halve (a)
+    return a/2
+end
